14916fe831c20e0582b3134dc8acb63d9a68c0ed
Aleksandr Ivanov <aleksand7@yandex.ru>
2021-02-19 23:37:54 +0300

    add material-ui

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,4 +1,5 @@
 import { CardType } from '../data/Type';
+import { Paper, Box } from '@material-ui/core';
 import WordAudio from './WordAudio';
 
 type CardProps = {
@@ -8,12 +9,18 @@ type CardProps = {
 function Card(props: CardProps) {
   const { word, transcription, translation } = props.card;
   return (
-    <div>
-      <p>{word}</p>
-      <p>{transcription}</p>
-      <p>{translation}</p>
-      <WordAudio word={word} />
-    </div>
+    <Paper elevation={3} style={{width: "50vw", height: "40vh", margin: "100px auto", padding: "50px"}}>
+      <div>
+        <Box display="flex">
+          <span style={{marginRight: "20px"}}>{word}</span>
+          <WordAudio word={word} />
+        </Box>
+        <p>{transcription}</p>
+        <p>
+          {translation}
+        </p>
+      </div>
+    </Paper>
   );
 }
 
--- src/components/WordAudio.jsx
+++ src/components/WordAudio.jsx
@@ -1,18 +1,20 @@
+import RecordVoiceOverIcon from '@material-ui/icons/RecordVoiceOver';
+
 const WordAudio = (props) => {
   const word = props.word.toLowerCase();
   const src = `https://wooordhunt.ru/data/sound/sow/us/${word}.mp3` || '#';
 
   return (
-    <div style={{ height: "100px", width: "100px", backgroundColor: "red" }} 
-      onClick={() => {
-        const el = document.getElementById(word);
-        el && el.play()
-      }}
-    >
+    <span>
       <audio id={word} preload="auto">
         <source src={src} type="audio/mpeg"></source>
       </audio>
-    </div>
+      <RecordVoiceOverIcon 
+        color="primary"
+        onClick={() => document.getElementById(word).play()}
+      >
+      </RecordVoiceOverIcon>
+    </span>
   )
 }
 
